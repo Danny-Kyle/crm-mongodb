8a9494e64a6f1d6706048eaeed0fa856c5623e6c
Danny-Kyle <dikedaniel7917@gmail.com>
2024-02-22 20:34:10 +0100

    frundry

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { MongoClient } from "mongodb";
 
-const express = require('express')
-const body = require('body-parser')
+const express = require('express');
+const body = require('body-parser');
 
 async function start(){
+    try{
+
+        const app = express();
+
+        const mongo = await MongoClient.connect('mongodb://localhost', {});
+
+        await mongo.connect();
+
+        app.db = mongo.db();
+
+        //body parser
+        app.use(body.json({
+            limit: '500kb'
+        }));
+    }
+    catch(error){
+        console.log(error);
+    }
 
 };
 
